906d1f67f31e9454b123e7af25ac24094ef13f72
lilinmail0523 <65453174+lilinmail0523@users.noreply.github.com>
2021-03-13 13:51:42 +0800

    update version

--- PreprocessingAndEmbedding.py
+++ PreprocessingAndEmbedding.py
@@ -1,12 +1,35 @@
 import json
 import nltk
 import string
-from gensim.models import KeyedVectors
+import gensim.downloader
 import inflect
 import time
 import numpy as np
 
+#t-sne reduction
+from sklearn.manifold import TSNE
+
+
 p = inflect.engine()
+
+#nltk pos list
+PosList = ["$", "''", "(", ")", ",", "--", ".", ":","CC", "CD", "DT", "EX", "FW", "IN", "JJ", "JJR", "JJS", "LS", "MD", 
+           "NN", "NNP", "NMPS", "NNS", "PDT", "POS", "PRP", "PRP$", "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH", "VB", 
+           "VBD", "VBG", "VBN","VBZ", "WDT", "WP", "WP$", "WRB"]
+
+NodeList = ["value", "agent", "condition", "theme", "theme_mod", "quant_mod", "co_quant", "null", "location", 
+            "whole", "source", "reference_time", "quant", "manner", "time", "cause", "+", "-"]
+
+NerList = ['B-FACILITY', 'B-GPE', 'B-GSP', 'B-LOCATION', 'B-ORGANIZATION', 'B-PERSON', 
+            'I-FACILITY', 'I-GPE', 'I-GSP', 'I-LOCATION', 'I-ORGANIZATION', 'I-PERSON',
+            'O']
+
+
+PosTagtoNum = {tag: i for i, tag in enumerate(PosList)} 
+NodeTagtoNum = {tag: i for i, tag in enumerate(NodeList)} 
+NerTagtoNum = {tag: i for i, tag in enumerate(NerList)} 
+
+wordEmbeddingSize = 100
 
 def ReadFile(input_file, model):
     with open(input_file, 'r') as f:
@@ -14,12 +37,14 @@ def ReadFile(input_file, model):
         for line in f:
             data_dic = json.loads(line)
             word_dict={}
-            POS = nltk.pos_tag(data_dic['tokens'])
+            pos = nltk.pos_tag(data_dic['tokens'])
+            Chunk = nltk.ne_chunk(pos)
+            PosNerList = nltk.chunk.tree2conlltags(Chunk)
 
             for node_index in data_dic['nodes']:
-                word_vector = np.zeros(300)
-                POS_vector = np.zeros(300)
-                node_vector = np.zeros(300)
+                word_vector = np.zeros(wordEmbeddingSize)
+                pos_vector = np.zeros(len(PosList))
+                ner_vector = np.zeros(len(NerList))
 
                 #range(i[0][0], i[0][1]) means the word that we interest
                 for index in range(node_index[0][0], node_index[0][1]):
@@ -29,35 +54,49 @@ def ReadFile(input_file, model):
                     word = word.replace(r'\/','')
                     word = word.replace(r',','')
 
-                    #the word which is not in dictionary
+                    #word2vector 
                     try:
                         word_vector += model[str(word)]
                     except:
-                        word_vector +=  np.zeros(300)
+                        word_vector +=  np.zeros(wordEmbeddingSize)
 
-                    #the POS which is not in dictionary
-                    try:
-                        POS_vector += model[POS[index][1]]
-                    except:
-                        word_vector +=  np.zeros(300)
+
+                    #POS tag for one hot encoding
+                    pos_feature =  np.zeros(len(PosList))
+                    posTagNumber = PosTagtoNum.get(PosNerList[index][1], -1)
+                    if posTagNumber != -1:
+                        pos_feature[posTagNumber] = 1.0
+                    
+                    pos_vector += pos_feature
+
+
+                    #NER tag for one hot encoding
+                    ner_feature =  np.zeros(len(NerList))
+
+                    #non-special name -> O
+                    nerTagNumber = NerTagtoNum.get(PosNerList[index][2], -1)
+                    
+                    ner_feature[nerTagNumber] = 1.0                    
+                    ner_vector += ner_feature
 
                 word_vector /= (node_index[0][1]- node_index[0][0])
-                POS_vector /= (node_index[0][1]- node_index[0][0])
-                
-                for key, value in node_index[1].items():
-                    #the node which is not in dictionary
-                    try:
-                        node_vector += model[key]
-                    except:
-                        node_vector = np.zeros(300)
+                pos_vector /= (node_index[0][1]- node_index[0][0])
+                ner_vector /= (node_index[0][1]- node_index[0][0])
 
-                word_dict.update({node_index[0][0]:[word_vector.tolist(), node_vector.tolist(), node_index[0][0], POS_vector.tolist()]})
+                for key, value in node_index[1].items():
+                    #node for one hot encoding
+                    node_feature =  np.zeros(len(NodeList))
+                    nodeTagNumber = NodeTagtoNum.get(key, -1)
+                    if nodeTagNumber != -1:
+                        node_feature[nodeTagNumber] = 1.0
+
+                word_dict.update({node_index[0][0]:[word_vector.tolist(), node_feature.tolist(), node_index[0][0], pos_vector.tolist(), ner_vector.tolist()]})
+                print(word_dict)
             #range(i[0][0], i[0][1]) means the relation that we interest
             for j in data_dic['edges']:
                 for key, value in j[2].items():
                     word_relations.append([word_dict[j[0][0]], word_dict[j[1][0]], key])
-
-    with open('Process' + input_file, 'w') as f:
+    with open('processed_' + input_file, 'w') as f:
         json.dump({'Relation':word_relations}, f)
 
     
@@ -66,8 +105,11 @@ def ReadFile(input_file, model):
 
 if __name__ == '__main__':
     start = time.time()
-    model = KeyedVectors.load_word2vec_format('wiki-news-300d-1M.vec')
+    model = gensim.downloader.load('glove-twitter-100')
     print('model load time: ', time.time() - start)
     TrainData = 'train.json'
     TestData = 'test.json'
-    ReadFile(TestData, model)
+
+
+
+    ReadFile(TrainData, model)
--- svc.py
+++ svc.py
@@ -13,27 +13,32 @@ import time
 def read_json(data_dir):
     """
     fetch data from preprocessed file
-    x[0] : word1 information
-    x[1] : word2 information
-    x[2] : relation between two word
+    sentence_id[0] : word1 information
+    sentence_id[1] : word2 information
+    sentence_id[2] : relation between two word
     word information:
     word[0] : word vector
     word[1] : node vector
     word[2] : location attribute
     word[3] : POS vector
+    word[4] : NER vector
     """
-    word_vector = []
-    pos_vector = []
-    node_vector = []
-    word2_pos = []
+    word= []
+    node= []
+    pos = []
+    ner = []
+    distance = []
+
     label = []
 
     with open(data_dir, 'r') as f:
         data_dic = json.loads(f.read())
         for sentence_id in data_dic['Relation']:
-            word_vector.append(sentence_id[0][0] + sentence_id[1][0])
-            node_vector.append(sentence_id[0][1] + sentence_id[1][1])
-            pos_vector.append(sentence_id[0][3] + sentence_id[1][3])
+            word.append(sentence_id[0][0] + sentence_id[1][0])
+            node.append(sentence_id[0][1] + sentence_id[1][1])
+            pos.append(sentence_id[0][3] + sentence_id[1][3])
+            ner.append(sentence_id[0][4] + sentence_id[1][4])
+            distance.append([sentence_id[0][2] - sentence_id[1][2], sentence_id[1][2] - sentence_id[0][2]])
 
             if sentence_id[2] == 'fact':
                 label.append(0)
@@ -42,7 +47,7 @@ def read_json(data_dir):
             elif sentence_id[2] == 'analogy':
                 label.append(2)
 
-    return word_vector, node_vector, pos_vector,  label
+    return word, node, pos,  ner, distance, label
 
 
 
@@ -70,6 +75,9 @@ def SVM(train_x, train_y, val_x, val_y, test_x, test_y, type):
     print('Test f1 score: ', f1)
     print('Total time of ' + type, time.time()- start)
 
+def CombinationTest(train, validation, test, train_label, val_label, test_label,type):
+    SVM(train,train_label,  validation, val_label, test, test_label, type)
+
 
 
 if __name__ == '__main__':
@@ -78,22 +86,41 @@ if __name__ == '__main__':
     warnings.filterwarnings("ignore")
 
     #train data
-    tr_word, tr_node, tr_pos,  tr_label = read_json('Processtrain.json')
+    tr_word, tr_node, tr_pos,  tr_ner, tr_distance, tr_label = read_json('processed_train.json')
     #test data
-    test_word, test_node, test_pos, test_label = read_json('Processtest.json')
+    test_word, test_node, test_pos, test_ner, test_distance, test_label = read_json('processed_test.json')
 
-    train_word, val_word, train_node, val_node, train_pos, val_pos, train_label, val_label = train_test_split(tr_word, tr_node, 
-                                                        tr_pos, tr_label, test_size=0.20, random_state=42)
+    train_word, val_word, train_node, val_node, train_pos, val_pos, train_ner, val_ner, train_distance,val_distance, train_label, val_label  = train_test_split(
+        tr_word, tr_node, tr_pos,  tr_ner, tr_distance, tr_label, test_size=0.20, random_state=42)
 
 
-    SVM(train_word,train_label,  val_word, val_label, test_word, test_label, 'word vector')
-    
-    SVM(train_node,train_label,  val_node, val_label, test_node, test_label, 'node vector')
 
-    SVM(train_pos,train_label,  val_pos, val_label, test_pos, test_label, 'pos vector')
+    #SVM(train_word,train_label,  val_word, val_label, test_word, test_label, 'word vector')
+    #CombinationTest(np.concatenate((train_word, train_distance), axis=1), np.concatenate((val_word, val_distance), axis=1), np.concatenate((test_word, test_distance),axis=1), train_label, val_label, test_label, "word + distance")
+   
+    #SVM(train_node,train_label,  val_node, val_label, test_node, test_label, 'node vector')
 
-    train_combine = np.concatenate((train_word, train_node, train_pos), axis=1)
-    val_combine = np.concatenate((val_word, val_node, val_pos), axis=1)
-    test_combine = np.concatenate((test_word, test_node, test_pos), axis=1)
-    SVM(train_combine,train_label,  val_combine, val_label, test_combine, test_label, 'Combine')
+    #SVM(train_pos,train_label,  val_pos, val_label, test_pos, test_label, 'pos vector')
+    #CombinationTest(np.concatenate((train_pos, train_distance), axis=1), np.concatenate((val_pos, val_distance), axis=1), np.concatenate((test_pos, test_distance),axis=1), train_label, val_label, test_label, "pos + distance")
+
+    '''
+    test combination of word2vector, node, pos, ner, distance
+    measure in 7 cases
+    pos + ner (+ distance)
+    word + pos (+ distance)
+    word + pos + ner (+ distance)
+    node + word + pos + ner + distance
+
+    '''
+    # pos + ner (+ distance) 
+    #CombinationTest(np.concatenate((train_pos, train_ner), axis=1), np.concatenate((val_pos, val_ner), axis=1), np.concatenate((test_pos, test_ner),axis=1), train_label, val_label, test_label, "pos + ner")
+    #CombinationTest(np.concatenate((train_pos, train_ner, train_distance), axis=1), np.concatenate((val_pos, val_ner, val_distance), axis=1), np.concatenate((test_pos, test_ner, test_distance),axis=1), train_label, val_label, test_label, "pos + ner + distance")
+    # word + pos (+ distance) 
+    #CombinationTest(np.concatenate((train_word, train_pos), axis=1), np.concatenate((val_word, val_pos), axis=1), np.concatenate((test_word, test_pos),axis=1), train_label, val_label, test_label, "word + pos")
+    #CombinationTest(np.concatenate((train_word, train_pos, train_distance), axis=1), np.concatenate((val_word, val_pos, val_distance), axis=1), np.concatenate((test_word, test_pos, test_distance),axis=1), train_label, val_label, test_label, "word + pos + distance")
+    # word + pos + ner (+ distance) 
+    #CombinationTest(np.concatenate((train_word, train_pos, train_ner), axis=1), np.concatenate((val_word, val_pos, val_ner), axis=1), np.concatenate((test_word, test_pos, test_ner),axis=1), train_label, val_label, test_label, "word + pos + ner")
+    #CombinationTest(np.concatenate((train_word, train_pos, train_ner, train_distance), axis=1), np.concatenate((val_word, val_pos, val_ner, val_distance), axis=1), np.concatenate((test_word, test_pos, test_ner, test_distance),axis=1), train_label, val_label, test_label, "word + pos + ner + distance")
+    # node + word + pos + ner + distance 
+    CombinationTest(np.concatenate((train_node, train_word, train_pos, train_ner, train_distance), axis=1), np.concatenate((val_node, val_word, val_pos, val_ner, val_distance), axis=1), np.concatenate((test_node, test_word, test_pos, test_ner, test_distance),axis=1), train_label, val_label, test_label, "node + word + pos + ner + distance ")
 
